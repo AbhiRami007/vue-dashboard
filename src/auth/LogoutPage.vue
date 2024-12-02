054364a9ca19a48d8c88460b3076a4db79285195
Abhirami007 <psabhirami015@gmail..com>
2024-12-01 20:00:15 -0500

    logout page added

---
+++ src/auth/LogoutPage.vue
@@ -0,0 +1,27 @@
+<script>
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+
+export default {
+  name: "LogoutPage",
+  setup() {
+    const router = useRouter();
+
+    // Clear user data and redirect
+    onMounted(() => {
+      // Clear the user data from localStorage
+      localStorage.removeItem("user");
+
+      // Redirect to the login page
+      router.push("/");
+    });
+
+    return {};
+  },
+};
+</script>
+
+<template>
+  <!-- No UI needed for logout -->
+  <div></div>
+</template>
